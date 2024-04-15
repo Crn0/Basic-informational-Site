dc8513b6987f0612f143dc975090f0115841ed41
Crno0 <123456789+odin@users.noreply.github.com>
2024-04-15 16:48:55 +0800

    Refactor code to use express

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-import { createServer } from 'node:http';
-import data from './src/data/data.js';
-import File from './src/helpers/read-write-file.js';
+import express from 'express';
+import router from './src/router/router.js';
 
-File.writeFile(data.index.file, data.index.content);
-File.writeFile(data.about.file, data.about.content);
-File.writeFile(data['contact-me'].file, data['contact-me'].content);
-File.writeFile(data.error.file, data.error.content);
+const app = express();
+const port = 8080
 
-const server = createServer((req, res) => {
-    const queryUrl = new URL(req.url, 'https://localhost:8080');
-    const path = queryUrl.pathname === '/' ? './src/html/index.html' : `./src/html/${queryUrl.pathname}.html`;
+app.use('/', router)
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    File.readFile(path, res, req.url);
-})
-
-server.listen(8080, 'localhost', () => {
-    console.log('listening')
-  });
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
 
